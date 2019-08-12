e2bf25539518a5038202962acd36663a8ebbdbae
markchen0308 <simonchen0308@gmail.com>
2019-08-12 16:15:05 +0800

    update

--- src/controlModebus.ts
+++ src/controlModebus.ts
@@ -418,7 +418,7 @@ export class ControlModbus {
                 })
         });
     }
-
+   
     //------------------------------------------------------------------------------------------
     async exeControlCmd(): Promise<boolean> {
         let cmd: DTCMD.iCmd;
